7e3d197d1dbbaa7098b7c73f84e7c13b75568c4d
Madeline Yi <sunhunfunbun@gmail.com>
2022-07-21 22:17:10 -0500

    created a create route and updated the links in the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
 
 function App() {
   return (
@@ -10,8 +11,11 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
             </Route>
           </Switch>
         </div>
