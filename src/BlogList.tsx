524763f8aeeeea63eb5bb94a8b5930470a0d1cda
Madeline Yi <sunhunfunbun@gmail.com>
2022-07-22 22:35:21 -0500

    created blog ids using route parameters and linked it to the blogs

--- src/BlogList.tsx
+++ src/BlogList.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs }) => {
   // const {blogs, title, handleDelete} = props;
 
@@ -5,8 +7,10 @@ const BlogList = ({ blogs }) => {
     <div className="home">
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
